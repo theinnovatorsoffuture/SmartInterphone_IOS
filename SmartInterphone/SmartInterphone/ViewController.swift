a1a832e74a19e0da7f29121cc427cbf483844c56
kais <kais.ladjemi@esprit.tn>
2019-02-07 14:30:32 -0800

    Initial Xcode Project

---
+++ SmartInterphone/SmartInterphone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartInterphone
+//
+//  Created by Ladjemi Kais on 2/7/19.
+//  Copyright © 2019 iof. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
